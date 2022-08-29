f6b5782db5e011fef51fdff59ef05b3787053fcc
Hassan Ndam <ndamfiverr@gmail.com>
2022-08-29 14:58:06 +0200

    modif css image header footer bouton

--- footer.php
+++ footer.php
@@ -1,4 +1,65 @@
+<!-- Footer -->
 
+<footer class="text-center mt-5 pt-3 border-top text-lg-start">
+
+  <section class="pb-1 p-3">
+    <div class="container text-center text-md-start mt-5">
+      <div class="row mt-3">
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <h6 class="text-uppercase fw-bold mb-4 titre-footer">
+            </i>Université PARIS-SACLAY 
+          </h6>
+          <a href="https://www.universite-paris-saclay.fr/" target="_blank">
+            <img class ="img-fluid mt-2"src="<?php echo (get_template_directory_uri() . '/assets/logo/logo-ups-white_1.svg') ?>" alt="Logo Université PARIS-SACLAY">
+          </a>
+          <p class="mt-5">3 rue Joliot Curie
+          Bâtiment Breguet
+          91190 Gif-sur-Yvette
+          </p>
+          <p>Standard : 01.69.15.67.50</p>
+        </div>
+
+        <div class="col-md-3 col-lg-2 col-xl-3 mx-auto mb-4">
+          <h6 class="text-uppercase fw-bold mb-4 titre-footer">
+            Contact
+          </h6>
+          <p>
+            <a href="https://www.universite-paris-saclay.fr/contact" class="text-reset" target="_blank" title="Contactez-nous">Contactez-nous</a>
+          </p>
+        </div>
+
+        <div class="col-md-3 col-lg-2 col-xl-3 mx-auto mb-4">
+          <h6 class="text-uppercase fw-bold mb-4 titre-footer">
+            Confidentialité
+          </h6>
+          <p>
+            <a href="https://www.universite-paris-saclay.fr/mentions-legales" class="text-reset" target="_blank">Mentions légales</a>
+          </p>
+        </div>
+
+        <div class="col-md-3 col-lg-2 col-xl-3 mx-auto mb-4">
+          <h6 class="text-uppercase fw-bold mb-4 titre-footer">
+            Nos réseaux sociaux
+          </h6>
+          <a href="https://www.facebook.com/UParisSaclay"><i class="fa-brands fa-facebook"></i></a>
+          <a href="https://www.linkedin.com/school/universit%C3%A9-paris-saclay/"><i class="fa-brands fa-linkedin"></i></a>
+          <a href="https://www.youtube.com/user/UParisSaclay"><i class="fa-brands fa-youtube"></i></a>
+          <a href="https://twitter.com/UnivParisSaclay"><i class="fa-brands fa-square-twitter"></i></a>
+          <a href="https://www.instagram.com/universite_paris_saclay/"><i class="fa-brands fa-instagram"></i></a>
+        </div>
+
+      </div>
+
+      <p class="texte-univ mb-4">L'Université Paris-Saclay est membre des réseaux LERU, CESAER et EUA</p> 
+
+    </div>
+</section>
+
+  <div class="text-center p-4 text-white" style="background-color: #1b1b1b;">
+    © <?= date('Y'); ?> Copyright @ Designed by:
+    <a class="text-reset fw-bold" href="https://paradisiak.com/" target="_blank">Paradisiak</a>
+  </div>
+</footer>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
--- header.php
+++ header.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo bloginfo("name");?></title>
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
     <link rel="icon" href="<?php echo (get_template_directory_uri() . '/assets/logo/logo_paris_saclay.svg') ?>" sizes="16x16 32x32 48x48 64x64">
 
 <?php wp_head(); ?>
@@ -13,3 +13,24 @@
 </head>
 <body>
 
+<header class="site-header bg-light shadow-sm ">
+    <nav class="navbar navbar-default navbar-expand d-flex container flex-column flex-md-row justify-content-between py-3">
+        <a class="navbar-brand" title="Aller sur la page d'accueil" href="<?php echo get_site_url(); ?>" >
+            <img src="<?php echo get_template_directory_uri(). '/assets/logo/paris-saclay.png'?>" alt="Logo Université Paris-Saclay" style="max-width:250px" class="img-fluid"> 
+        </a>
+    <div class="d-flex">
+        <a href="#" target="_blank">
+            <button type="button" class="btn btn-primary mobilite-btn" onclick="this.blur();">Mobilité interne</button>
+        </a>
+        <a href="#" target="_blank">
+            <button type="button" class="btn btn-primary" onclick="this.blur();">Candidature spontanée</button>
+        </a>
+    </div>
+    </nav>
+
+</header>
+
+
+<div class="position-relative p-3 p-md-5  overflow-hidden header-image">
+   <!-- header image    -->
+</div>
