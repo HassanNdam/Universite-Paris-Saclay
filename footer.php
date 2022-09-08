9da5296d25286e9a2154d8d4b8b57a63aad89c2f
Hassan Ndam <ndamfiverr@gmail.com>
2022-09-08 10:28:43 +0200

    Add link tous les métiers

--- footer.php
+++ footer.php
@@ -1,69 +1,85 @@
 <!-- Footer -->
 
 <footer class="text-center mt-5 pt-3 border-top text-lg-start">
-  <section class="pb-1 p-3">
-    <div class="container text-center text-md-start mt-5">
-      <div class="row mt-3">
-        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-          <h6 class="text-uppercase fw-bold mb-4 titre-footer">
-            </i>PARIS-SACLAY 
-          </h6>
-          <a href="https://www.universite-paris-saclay.fr/" target="_blank">
-            <img class ="img-fluid mt-2"src="<?php echo (get_template_directory_uri() . '/assets/logo/logo-ups-white_1.svg') ?>" alt="Logo Université PARIS-SACLAY">
-          </a>
-          <p class="mt-5">3 rue Joliot Curie
-          Bâtiment Breguet
-          91190 Gif-sur-Yvette
-          </p>
-          <p>Standard : 01.69.15.67.50</p>
+    <section class="pb-1 p-3">
+        <div class="container text-center text-md-start mt-5">
+            <div class="row mt-3">
+                <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                    <h6 class="text-uppercase fw-bold mb-4 titre-footer">
+                         </i><!--UNIVERSITÉ--> PARIS-SACLAY 
+                    </h6>
+                    <a href="https://www.universite-paris-saclay.fr/" target="_blank">
+                        <img class="img-fluid mt-2"
+                            src="<?php echo(get_template_directory_uri() . '/assets/logo/logo-ups-white_1.svg') ?>"
+                            alt="Logo Université PARIS-SACLAY">
+                    </a>
+                    <p class="mt-5">3 rue Joliot Curie
+                        Bâtiment Breguet
+                        91190 Gif-sur-Yvette
+                    </p>
+                    <p>Standard : 01.69.15.67.50</p>
+                </div>
+
+                <div class="col-md-3 col-lg-2 col-xl-3 mx-auto mb-4">
+                    <h6 class="text-uppercase fw-bold mb-4 titre-footer">
+                        Confidentialité
+                    </h6>
+                    <p>
+                        <a href="https://www.universite-paris-saclay.fr/mentions-legales" class="text-reset"
+                            target="_blank">Mentions légales</a>
+                    </p>
+                </div>
+                <div class="col-md-3 col-lg-2 col-xl-3 mx-auto mb-4">
+                    <h6 class="text-uppercase fw-bold mb-4 titre-footer">
+                        Contact
+                    </h6>
+                    <p>
+                        <a href="https://www.universite-paris-saclay.fr/contact" class="text-reset" target="_blank"
+                            title="Contactez-nous">Contactez-nous</a>
+                    </p>
+                    <p>
+                        <a href="https://data.enseignementsup-recherche.gouv.fr/pages/referens/?flg=fr" class="text-reset" target="_blank"
+                            title="Tous les métiers de l'enseignement supérieur et la recherche">
+                            <button type="button" class="btn bg-transparent border border-2 text-white">Listes des
+                                métiers</button>
+                        </a>
+                    </p>
+                </div>
+
+
+
+                <div class="col-md-3 col-lg-2 col-xl-3 mx-auto mb-4">
+                    <h6 class="text-uppercase fw-bold mb-4 titre-footer">
+                        Nos réseaux sociaux
+                    </h6>
+                    <a href="https://www.facebook.com/UParisSaclay"><i class="fa-brands fa-facebook"></i></a>
+                    <a href="https://www.linkedin.com/school/universit%C3%A9-paris-saclay/"><i
+                            class="fa-brands fa-linkedin"></i></a>
+                    <a href="https://www.youtube.com/user/UParisSaclay"><i class="fa-brands fa-youtube"></i></a>
+                    <a href="https://twitter.com/UnivParisSaclay"><i class="fa-brands fa-square-twitter"></i></a>
+                    <a href="https://www.instagram.com/universite_paris_saclay/"><i
+                            class="fa-brands fa-instagram"></i></a>
+                </div>
+
+            </div>
+
+            <p class="texte-univ mb-4 fst-italic">L'Université Paris-Saclay est membre des réseaux LERU, CESAER et EUA
+            </p>
+
         </div>
+    </section>
 
-        <div class="col-md-3 col-lg-2 col-xl-3 mx-auto mb-4">
-          <h6 class="text-uppercase fw-bold mb-4 titre-footer">
-            Contact
-          </h6>
-          <p>
-            <a href="https://www.universite-paris-saclay.fr/contact" class="text-reset" target="_blank" title="Contactez-nous">Contactez-nous</a>
-          </p>
-        </div>
-
-        <div class="col-md-3 col-lg-2 col-xl-3 mx-auto mb-4">
-          <h6 class="text-uppercase fw-bold mb-4 titre-footer">
-            Confidentialité
-          </h6>
-          <p>
-            <a href="https://www.universite-paris-saclay.fr/mentions-legales" class="text-reset" target="_blank">Mentions légales</a>
-          </p>
-        </div>
-
-        <div class="col-md-3 col-lg-2 col-xl-3 mx-auto mb-4">
-          <h6 class="text-uppercase fw-bold mb-4 titre-footer">
-            Nos réseaux sociaux
-          </h6>
-          <a href="https://www.facebook.com/UParisSaclay"><i class="fa-brands fa-facebook"></i></a>
-          <a href="https://www.linkedin.com/school/universit%C3%A9-paris-saclay/"><i class="fa-brands fa-linkedin"></i></a>
-          <a href="https://www.youtube.com/user/UParisSaclay"><i class="fa-brands fa-youtube"></i></a>
-          <a href="https://twitter.com/UnivParisSaclay"><i class="fa-brands fa-square-twitter"></i></a>
-          <a href="https://www.instagram.com/universite_paris_saclay/"><i class="fa-brands fa-instagram"></i></a>
-        </div>
-
-      </div>
-
-      <p class="texte-univ mb-4 fst-italic">L'Université Paris-Saclay est membre des réseaux LERU, CESAER et EUA</p> 
-
+    <div class="text-center p-4 text-white" style="background-color: #1b1b1b;">
+        © <?= date('Y'); ?> Copyright @ Designed by:
+        <a class="text-reset fw-bold" href="https://paradisiak.com/" target="_blank">Paradisiak</a>
     </div>
-</section>
-
-  <div class="text-center p-4 text-white" style="background-color: #1b1b1b;">
-    © <?= date('Y'); ?> Copyright @ Designed by:
-    <a class="text-reset fw-bold" href="https://paradisiak.com/" target="_blank">Paradisiak</a>
-  </div>
 </footer>
 
 
-<?php 
-wp_footer()  
-?>
+<?php
+wp_footer()
+            ?>
 
 </body>
+
 </html>
