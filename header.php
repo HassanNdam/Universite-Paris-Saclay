fee632a3bfd3be3af3a8c7872887b47f617d3d7b
Hassan Ndam <ndamfiverr@gmail.com>
2022-09-09 10:01:04 +0200

    Add favicone

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo bloginfo("name");?></title>
-    <link rel="icon" href="<?php echo(get_template_directory_uri() . '/assets/logo/logo_paris_saclay.svg') ?>"
+    <link rel="icon" href="<?php echo(get_template_directory_uri() . '/assets/favicone/favicon-u-prune.png') ?>"
         sizes="16x16 32x32 48x48 64x64">
 
     <?php wp_head(); ?>
@@ -16,7 +16,7 @@
 <body>
     <?php
 
-$jobcontract = get_post_custom_values('job_contract')[0];
+    $jobcontract = get_post_custom_values('job_contract')[0];
     $joblocation = get_post_custom_values('job_location')[0];
     $jobcategory = get_post_custom_values('job_category')[0];
     $jobbody = get_post_custom_values('job_body')[0];
@@ -27,16 +27,16 @@ $jobcontract = get_post_custom_values('job_contract')[0];
 
     <header class="site-header">
         <nav
-            class="container navbar navbar-default navbar-expand d-flex  flex-column flex-md-row justify-content-between py-3">
+            class="container navbar navbar-default navbar-expand d-flex flex-column flex-md-row justify-content-between py-3">
             <a class="navbar-brand" title="Aller sur la page d'accueil" href="<?php echo get_site_url(); ?>">
                 <img src="<?php echo get_template_directory_uri(). '/assets/logo/paris-saclay.png'?>"
                     alt="Logo Université Paris-Saclay" style="max-width:250px" class="img-fluid">
             </a>
-            <div class="d-flex">
+            <div class="text-center mt-4 mt-lg-0 "> 
                 <a href="#" target="_blank">
                     <button type="button" class="btn mobilite-btn" onclick="this.blur();">Mobilité interne</button>
                 </a>
-                <a href="https://jobaffinity.fr/apply/zpmce5gaejoh4tzlip" target="_blank">
+                <a href="https://jobaffinity.fr/apply/zpmce5gaejoh4tzlip" target="_blank" class="">
                     <button type="button" class="btn btn-primary espace-buton-center" onclick="this.blur();">
                         Candidature spontanée</button>
                 </a>
@@ -58,7 +58,7 @@ if (is_front_page()) :
 else :
     ?>
     <div class="position-relative  overflow-hidden header-image-post">
-        <?php    image_post_change($jobcategory); ?>
+        <?php    image_post_change($joblocation); ?>
     </div>
     <?php
 endif;
