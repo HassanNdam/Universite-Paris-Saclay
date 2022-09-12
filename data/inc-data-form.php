6b5090131ec3b319dcc3a075f4103881067090b9
Hassan Ndam <ndamfiverr@gmail.com>
2022-09-12 15:51:36 +0200

    Add data for seacrch search engine

---
+++ data/inc-data-form.php
@@ -0,0 +1,47 @@
+<?php 
+
+
+// Récup job Category
+
+
+define("JOBCATEGORY", [
+  "Catégorie A : cadres",
+  "Catégorie B : techniciens et encadrement intermédiaire",
+  "Catégorie C : fonctions d'exécution"
+]);
+
+
+// Récup job Contract
+
+define ("JOBCONTRACT", [
+  "Titulaire de la fonction publique", 
+  "Titulaire de la fonction publique ou CDD" 
+]);
+
+
+// Récup job Branch 
+
+define ("JOBBRANCH",[
+  "BAP A : Sciences du vivant, de la terre et de l'environnement",
+  "BAP B : Sciences chimiques et Sciences des matériaux",
+  "BAP C : Sciences de l'Ingénieur et instrumentation scientifique",
+  "BAP D : Sciences Humaines et Sociales",
+  "BAP E : Informatique, Statistiques et Calcul scientifique",
+  "BAP F : Culture, Communication, Production et diffusion des savoirs",
+  "BAP G : Patrimoine immobilier, Logistique, Restauration et Prévention",
+  "BAP J : Gestion et Pilotage"
+]);
+
+
+// Récup job Body 
+
+define("JOBBODY",[
+  "IGR Attaché-e principal Conservateur", 
+  "IGE Attaché-e Bibliothécaire", 
+  "ASI", 
+  "TECH SAENES BIBAS", 
+  "ATRF ADJAENES Magasinier"
+]); 
+
+
+var_dump(JOBCONTRACT, JOBCATEGORY, JOBBODY, JOBBRANCH);
