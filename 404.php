e4f96ef31cb5fac1eb23cdac0fe5dacfefd04a16
Hassan Ndam <ndamfiverr@gmail.com>
2022-09-14 12:34:37 +0200

    Add 404 page

---
+++ 404.php
@@ -0,0 +1,23 @@
+<?php 
+
+get_header(); 
+
+?>
+
+<?php if(is_404()) : ?>
+<div class="container d-flex align-items-center justify-content-center mt-5 mb-5 shadow-sm p-5">
+    <div class="text-center">
+        <h1 class="display-1 fw-bold text-404 ">404</h1>
+        <p class="fs-3 mt-3"> <span class="text-warning">Oups!</span> Page non trouvée.</p>
+        <p class="lead">
+            La page que vous recherchez n'exitse pas.
+        </p>
+        <a href="<?= get_site_url(); ?>" class="btn btn-primary mt-3 mb-3">Retour à l'accueil</a>
+    </div>
+</div>
+
+<?php 
+endif ;
+get_footer(); 
+
+?>
