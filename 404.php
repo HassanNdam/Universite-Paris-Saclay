fb775eb74fe757fdf39e5b276b0631a70f54739e
Hassan Ndam <ndamfiverr@gmail.com>
2022-09-15 16:36:27 +0200

    Remove oups add Désolé

--- 404.php
+++ 404.php
@@ -8,7 +8,7 @@ get_header();
 <div class="container d-flex align-items-center justify-content-center mb-5 shadow-sm p-5">
     <div class="text-center">
         <h1 class="display-1 fw-bold text-404 ">404</h1>
-        <p class="fs-3 mt-3"> <span class="text-warning">Oups!</span> Page non trouvée.</p>
+        <p class="fs-3 mt-3"> <span class="text-warning">Désolé!</span> Page non trouvée.</p>
         <p class="lead">
             La page que vous recherchez n'exitse pas.
         </p>
