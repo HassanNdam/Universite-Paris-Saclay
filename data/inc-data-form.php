94774465be7bef809a360c542593f46f885fd5c3
Hassan Ndam <ndamfiverr@gmail.com>
2022-09-16 11:08:10 +0200

    add false (case insensitive) to define

--- data/inc-data-form.php
+++ data/inc-data-form.php
@@ -1,27 +1,26 @@
-<?php 
+<?php
+
 
 
 // Récup job Category
-
 
 define("JOBCATEGORY", [
   "Catégorie A : cadres",
   "Catégorie B : techniciens et encadrement intermédiaire",
   "Catégorie C : fonctions d'exécution"
-]);
-
+], false);
 
 // Récup job Contract
 
-define ("JOBCONTRACT", [
-  "Titulaire de la fonction publique", 
-  "Titulaire de la fonction publique ou CDD" 
-]);
+define("JOBCONTRACT", [
+  "Titulaire de la fonction publique",
+  "Titulaire de la fonction publique ou CDD"
+], false);
 
 
-// Récup job Branch 
+// Récup job Branch
 
-define ("JOBBRANCH",[
+define("JOBBRANCH", [
   "BAP A : Sciences du vivant, de la terre et de l'environnement",
   "BAP B : Sciences chimiques et Sciences des matériaux",
   "BAP C : Sciences de l'Ingénieur et instrumentation scientifique",
@@ -30,18 +29,14 @@ define ("JOBBRANCH",[
   "BAP F : Culture, Communication, Production et diffusion des savoirs",
   "BAP G : Patrimoine immobilier, Logistique, Restauration et Prévention",
   "BAP J : Gestion et Pilotage"
-]);
+], false);
 
+// Récup job Body
 
-// Récup job Body 
-
-define("JOBBODY",[
-  "IGR Attaché-e principal Conservateur", 
-  "IGE Attaché-e Bibliothécaire", 
-  "ASI", 
-  "TECH SAENES BIBAS", 
+define("JOBBODY", [
+  "IGR Attaché-e principal Conservateur",
+  "IGE Attaché-e Bibliothécaire",
+  "ASI",
+  "TECH SAENES BIBAS",
   "ATRF ADJAENES Magasinier"
-]); 
-
-
-var_dump(JOBCONTRACT, JOBCATEGORY, JOBBODY, JOBBRANCH);
+], false);
