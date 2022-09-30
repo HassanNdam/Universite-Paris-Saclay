88253762c3ebdfec39997eeddc02c6dba54b4ea0
Hassan Ndam <ndamfiverr@gmail.com>
2022-09-30 15:49:08 +0200

    Add social share (Paused)

--- functions.php
+++ functions.php
@@ -274,3 +274,52 @@ function change_title_browser_top(){
             return $search_title; 
         }
 }
+
+
+//Social Sharing function 
+
+// Function to handle the thumbnail request
+// function get_the_post_thumbnail_src($img)
+// {
+//   return (preg_match('~\bsrc="([^"]++)"~', $img, $matches)) ? $matches[1] : '';
+// }
+// function wpvkp_social_buttons($content) {
+//     global $post;
+//     if(is_singular() || is_home()){
+    
+//         // Get current page URL 
+//         $sb_url = urldecode(get_permalink());
+ 
+//         // Get current page title
+//         $sb_title = str_replace( ' ', '%20', get_the_title());
+        
+//         // Get Post Thumbnail for pinterest
+//         $sb_thumb = get_the_post_thumbnail_src(get_the_post_thumbnail());
+ 
+//         // Construct sharing URL without using any script
+//         $twitterURL = 'https://twitter.com/intent/tweet?text='.$sb_title.'&amp;url='.$sb_url.'&amp;via=wpvkp';
+//         $whatsappURL = 'whatsapp://send?text='.$sb_title . ' ' . $sb_url;
+//         $linkedInURL = 'https://www.linkedin.com/shareArticle?mini=true&url='.$sb_url.'&amp;title='.$sb_title;
+
+ 
+//         // Add sharing button at the end of page/page content
+//         $content .= '<div class="social-box"><div class="social-btn">PARTAGER ';
+//         $content .= '<a class="col-1 sbtn s-twitter" href="'. $twitterURL .'" target="_blank" rel="nofollow"><span>Twitter</span></a>';
+//         $content .= '<a class="col-2 sbtn s-whatsapp" href="'.$whatsappURL.'" target="_blank" rel="nofollow"><span>WhatsApp</span></a>';
+//         $content .= '<a class="col-2 sbtn s-linkedin" href="'.$linkedInURL.'" target="_blank" rel="nofollow"><span>LinkedIn</span></a>';
+//         $content .= '</div></div>';
+        
+//         return $content;
+//     }else{
+//         // if not a post/page then don't include sharing button
+//         return $content;
+//     }
+// };
+// // Enable the_content if you want to automatically show social buttons below your post.
+
+//  add_filter( 'the_content', 'wpvkp_social_buttons');
+
+// // This will create a wordpress shortcode [social].
+// // Please it in any widget and social buttons appear their.
+// // You will need to enabled shortcode execution in widgets.
+// add_shortcode('social','wpvkp_social_buttons');
